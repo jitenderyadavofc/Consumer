49215731719e4b4a8771d5a980235bd5a30eb995
jitenderyadavinogen <jitender@birlasoft.com>
2024-02-27 21:15:18 +0530

    changed secret key

--- terraform/project_athenix/providers.tf
+++ terraform/project_athenix/providers.tf
@@ -1,8 +1,8 @@
 provider "aws" {
   #alias      = "account_prod"
   region     = var.region
-  access_key = "AKIA3Y2WBELOLNCRS4NL"
-  secret_key = "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"
+  access_key = "AKIA3FLD5QOYL6IF4LOT"
+  secret_key = "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"
 }
 
 # provider "aws" {
