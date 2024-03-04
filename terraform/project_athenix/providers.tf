ad09bc87b651aaa10ec0fed15048d58853036fd2
jitenderyadavofc <157213132+jitenderyadavofc@users.noreply.github.com>
2024-03-04 22:14:13 +0530

    Update providers.tf

--- terraform/project_athenix/providers.tf
+++ terraform/project_athenix/providers.tf
@@ -1,8 +1,8 @@
 provider "aws" {
   #alias      = "account_prod"
   region     = var.region
-  access_key = "AKIA3FLD5QOYL6IF4LOT"
-  secret_key = "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"
+ # access_key = "AKIA3FLD5QOYL6IF4LOT"
+ # secret_key = "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"
 }
 
 # provider "aws" {
